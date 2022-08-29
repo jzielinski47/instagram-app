69e4d7a22b8189caca8a5c79c32574306eb4efe6
Deluxe Duck <deluxe.duck@yahoo.com>
2022-08-29 16:03:38 +0200

    user-connection-update

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const port = 3000;
 const imageRouter = require("./app/routes/imageRouter");
 const tagsRouter = require("./app/routes/tagsRouter");
 const filterRouter = require("./app/routes/filterRouter");
+const userRouter = require("./app/routes/userRouter");
 
 const server = http.createServer(async (req, res) => {
-
-    console.log(req.url)
 
     if (req.url.search("/api/photos") != -1) { await imageRouter(req, res) }
     else if (req.url.search("/api/tags") != -1) { await tagsRouter(req, res) }
     else if (req.url.search("/api/filters") != -1) { await filterRouter(req, res) }
+    else if (req.url.search("/api/users") != -1) { await userRouter(req, res) }
 
 })
 
